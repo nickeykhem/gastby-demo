452a8959438a394fda53af2db3beb9aafe51613e
Nickey Khem <nike121@gmail.com>
2021-07-13 19:59:26 +0800

    modded gitignore to enable public files sync

---
+++ public/component---src-pages-index-js-c0b2b80436f4e54bb2f8.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{704:function(e,t,l){"use strict";l.r(t),l.d(t,{default:function(){return r}});var n=l(294);function r(){return n.createElement("section",null,n.createElement("div",null,n.createElement("h2",null,"Testing"),n.createElement("h3",null,"Demo website"),n.createElement("p",null,"This is a demo website to learn how to use Gatsby.")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-c0b2b80436f4e54bb2f8.js.map
